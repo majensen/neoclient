8e934cc13d258813e031e22519071b75c9e6295e
Mark A. Jensen <maj-git@fortinbras.us>
2020-05-31 22:55:03 -0400

    prep for the world

--- lib/Neo4j/Client.pm
+++ lib/Neo4j/Client.pm
@@ -26,7 +26,7 @@ sub dir { realpath(module_dir(__PACKAGE__)) }
 
 =head1 NAME
 
-Neo4j::Client - build libneo4j-client on your machine
+Neo4j::Client - Build and use the libneo4j-client library
 
 =head1 SYNOPSIS
 
@@ -39,13 +39,28 @@ Neo4j::Client - build libneo4j-client on your machine
 Chris Leishman's
 L<libneo4j-client|https://github.com/cleishm/libneo4j-client> is a C
 library for communication with a Neo4j (<v4.0) server via the Bolt
-protocol. Installing this module will attempt to build the library on your
-machine for use with L<Neo4j::Bolt>.
+protocol. Installing this module will attempt to build the (static)
+library on your machine for use with L<Neo4j::Bolt>.
 
 =head1 AUTHOR
 
  Mark A. Jensen < majensen -at- cpan -dot- org >
  CPAN: MAJENSEN
+
+=head1 LICENSE
+
+This packaging software is Copyright (c) 2020 by Mark A. Jensen.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+The L<libneo4j-client|https://github.com/clieshm/libneo4j-client> software 
+is Copyright (c) by Chris Leishman. 
+
+It is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
 
 =cut
 
