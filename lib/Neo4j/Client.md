3f700f17e76073051a66c05f352587d98c0cb6a3
Mark A. Jensen <maj-git@fortinbras.us>
2020-12-28 22:53:13 -0500

    feat(tools) - bundle specific autotools known to work
    and set them up before the build (using Alien of course)

--- lib/Neo4j/Client.md
+++ lib/Neo4j/Client.md
@@ -21,11 +21,14 @@ library for communication with a Neo4j server via the Bolt
 protocol. 
 
 Installing this module will attempt to build the API portion of the
-library on your machine. `libneo4j-client`'s interactive shell and 
-documentation are not built.
+library on your machine. `libneo4j-client`'s interactive shell and
+documentation are not built. The install process will use the GNU
+autotools `autoconf-2.69`, `automake-1.16.3`, and `m4-1.4.18-patched`
+which are bundled with this distro and are known to work on this library.
+(These are required to build from ./configure for `libneo4j-client`.)
 
 Thanks to the miracle of [Alien::Build](https://metacpan.org/pod/Alien::Build), the library should always
-contain OpenSSL support. 
+contain OpenSSL support.
 
 # SEE ALSO
 
@@ -54,3 +57,8 @@ is Copyright (c) by Chris Leishman.
 It is free software, licensed under:
 
     The Apache License, Version 2.0, January 2004
+
+The bundled GNU Autotools autoconf, automake, and m4 are free software, 
+licensed under:
+
+    The GNU General Public License, Version 3, June 2007
--- lib/Neo4j/Client.pm
+++ lib/Neo4j/Client.pm
@@ -28,11 +28,15 @@ library for communication with a Neo4j server via the Bolt
 protocol. 
 
 Installing this module will attempt to build the API portion of the
-library on your machine. C<libneo4j-client>'s interactive shell and 
-documentation are not built.
+library on your machine. C<libneo4j-client>'s interactive shell and
+documentation are not built. The install process will use the GNU
+autotools C<autoconf-2.69>, C<automake-1.16.3>, and C<m4-1.4.18-patched>
+which are bundled with this distro and are known to work on this library.
+(These are required to build from ./configure for C<libneo4j-client>.)
 
 Thanks to the miracle of L<Alien::Build>, the library should always
-contain OpenSSL support. 
+contain OpenSSL support.
+
 
 =head1 SEE ALSO
 
@@ -45,7 +49,7 @@ L<Neo4j::Bolt>.
 
 =head1 ACKNOWLEDGMENT
 
-Thanks L<ETJ|https://metacpanorg/author/ETJ> (a.k.a mohawk) for beaming me aboard.
+Thanks L<ETJ|https://metacpan.org/author/ETJ> (a.k.a mohawk) for beaming me aboard.
 
 =head1 LICENSE
 
@@ -61,6 +65,11 @@ is Copyright (c) by Chris Leishman.
 It is free software, licensed under:
 
   The Apache License, Version 2.0, January 2004
+
+The bundled GNU Autotools autoconf, automake, and m4 are free software, 
+licensed under:
+
+  The GNU General Public License, Version 3, June 2007
 
 =cut
 
